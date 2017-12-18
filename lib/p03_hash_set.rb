b21084b6d60587128aa4379308b08a4f472cd84e
Tim Kharshan <timkharshan@hotmail.com>
2017-12-18 13:53:55 -0800

    3 done

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -3,20 +3,28 @@ require_relative 'p02_hashing'
 class HashSet
   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    @store[(num.hash)%num_buckets][0] = true
+    @count = count + 1
+    if (@count == num_buckets) 
+      resize! 
+    end
+
   end
 
-  def include?(key)
+  def remove(num)
+    @store[(num.hash)%num_buckets][0]=false
+    @count = @count - 1
   end
 
-  def remove(key)
+  def include?(num)
+    @store[(num.hash)%num_buckets][0] || false
   end
-
   private
 
   def [](num)
@@ -27,6 +35,11 @@ class HashSet
     @store.length
   end
 
-  def resize!
+  def resize! 
+    temp = @store
+    @count = 0
+     
+    @store = Array.new(@store.length*2) { Array.new }
+    temp.each_with_index{|item, index| item[0]? self.insert(index) : false}
   end
 end
