142754a600b7d87688b0bd9fc9f983a7adc1d130
Tim Kharshan <timkharshan@hotmail.com>
2017-12-18 13:32:42 -0800

    2 done

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,15 @@ end
 
 class Array
   def hash
+    self.empty? ? 0.hash : self.flatten.reduce{|sum,el| sum=el.to_i.hash}
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.split('').each_with_index{|el,idx| sum += idx*el.ord}
+    sum.hash
   end
 end
 
@@ -16,6 +20,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
+    #what?
     0
   end
 end
